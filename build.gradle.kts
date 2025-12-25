8446a6c05351d25e074667773c088287b28ac749
AlvinQID <contact@alvinq.my.id>
2025-12-25 16:17:25 +0700

    fix

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "id.my.nexcaise"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+}
+
+dependencies {
+    implementation 'org.json:json:20240303'
+    compileOnly 'com.google.android:android:4.1.1.4'
+    implementation 'org.jetbrains:annotations:24.1.0'
+}
